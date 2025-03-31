d9310becf62e31f1d6f98421648da49cca28e9cb
Ramya Prasanna <prasannr@purdue.edu>
2025-03-31 19:52:35 -0400

    update

--- clean_data.py
+++ clean_data.py
@@ -1,15 +1,17 @@
 import pandas as pd
 
-df = pd.read_csv(r'C:\coding_work\GitHub\neurotech_fnir_mind\data\rest15_averaged.csv')
+file_path = r'C:\coding_work\GitHub\neurotech_fnir_mind\data\rest15_averaged.csv'
+
+df = pd.read_csv(file_path)
 
 print("Before filtering:")
 print(df.head())
 
 df = df.iloc[:, 0:3]
 
-df = df[(abs(df['HbO_avg']) >= 1e-07) & (abs(df['HbR_avg']) > 1e-07)]
+df = df[(abs(df['HbO_avg']) > 1e-07) & (abs(df['HbR_avg']) > 1e-07)]
 
 print("After filtering:")
 print(df.head())
 
-df.to_csv(r'C:\coding_work\GitHub\neurotech_fnir_mind\data\rest15_averaged.csv', index=False)
+df.to_csv(file_path, index=False)
