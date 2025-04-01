62ef46fb1d196eff70851f99f4638bfa0d5a07eb
Ramya Prasanna <prasannr@purdue.edu>
2025-03-31 20:00:04 -0400

    update 2

--- clean_data.py
+++ clean_data.py
@@ -1,17 +1,22 @@
 import pandas as pd
 
-file_path = r'C:\coding_work\GitHub\neurotech_fnir_mind\data\rest15_averaged.csv'
+file_path = [r'C:\coding_work\GitHub\neurotech_fnir_mind\data\rest5_averaged.csv',
+             r'C:\coding_work\GitHub\neurotech_fnir_mind\data\rest15_averaged.csv',
+             r'C:\coding_work\GitHub\neurotech_fnir_mind\data\rest17_averaged.csv',
+             r'C:\coding_work\GitHub\neurotech_fnir_mind\data\rest18_averaged.csv',
+             r'C:\coding_work\GitHub\neurotech_fnir_mind\data\rest20_averaged.csv']
 
-df = pd.read_csv(file_path)
+for i in range(5):
+    df = pd.read_csv(file_path[i])
 
-print("Before filtering:")
-print(df.head())
+    print("Before filtering:")
+    print(df.head())
 
-df = df.iloc[:, 0:3]
+    df = df.iloc[:, 0:3]
 
-df = df[(abs(df['HbO_avg']) > 1e-07) & (abs(df['HbR_avg']) > 1e-07)]
+    df = df[(abs(df['HbO_avg']) > 1e-07) & (abs(df['HbR_avg']) > 1e-07)]
 
-print("After filtering:")
-print(df.head())
+    print("After filtering:")
+    print(df.head())
 
-df.to_csv(file_path, index=False)
+    df.to_csv(file_path[i], index=False)
